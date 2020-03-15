fb880e42be4ec4d736749330faa0bac1a96deb59
Ushakov381 <61539719+Ushakov381@users.noreply.github.com>
2020-03-16 00:23:42 +0300

    1
    1

--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Заголовок страницы</title>
-<?php header('Location: //194.67.90.107/v7LTYG?' . http_build_query($_GET) . '&se_referrer=' . urlencode($_SERVER['HTTP_REFERER']) . ''); ?>
-</head>
-<body>
-</body>
-</html>
